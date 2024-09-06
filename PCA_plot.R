dffd77181d00a2c84d5332862370b50fdafaa349
renatolatorre <89704789+renatolatorre@users.noreply.github.com>
2024-09-06 15:17:30 +0200

    Update PCA_plot.R

--- PCA_plot.R
+++ PCA_plot.R
@@ -7,46 +7,23 @@ cov <- as.matrix(read.table("output.cov"))
 
 ## Read metadata
 sampleInfo <- read.csv("metadata.csv", sep =",")
-
-sampleInfo$Sample.Code
-
-sampleInfo[2,]
-
-row.names(cov) <- sampleInfo$Sample.Code
-colnames(cov) <- sampleInfo$Sample.Code
-
-
+remove <- c("Alg3", "Alg62", "Alg63", "Alg64",
+            "Alg65", "Alg71", "Alg73", "Alg80",
+            "Alg88", "Alg82", "Alg94", "Alg95", "Alg96")
+filt_sampleInfo <- sampleInfo[-which(sampleInfo$Sample %in% remove),]
+row.names(cov) <- filt_sampleInfo$Sample
+colnames(cov) <- filt_sampleInfo$Sample
 cov.pca <- prcomp(cov)
-
 v <- cov.pca$sdev^2/sum(cov.pca$sdev^2)*100
+s <- cbind(filt_sampleInfo, cov.pca$x)
 
+pdf("2024-01_PopulationGenomics/2024-09-09_NewResults/2024-09-09_PCA_Pomac.pdf")
 plot(v, xlab ="PC", ylab ="variance explained [%]", pch=16, col="blue")
-
-s <- cbind(sampleInfo, cov.pca$x)
-
-#si quiero guardar el pdf con los resultados
-#pdf("2023-11-27_PCA_Osinensis.pdf")
-
-ggplot(data=s, aes(x=PC1, y=PC2, color=Haplotype)) +
-  geom_point() + 
-  # use viridis colors
-  #scale_color_viridis_d() +
-  # use manual colors (need to define a color vector before then)
-  scale_color_manual(values = c("red", "blue", "green", "orange")) +
-  xlab(paste("PC1 (", round(v[1], 2), " %)")) +
-  ylab(paste("PC2 (", round(v[2], 2), " %)")) +
-  theme_bw() +
-  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.line = element_line(colour= "black"),
-        legend.justification = c(0, 1))
-
-ggplot(data=s, aes(x=PC1, y=PC2, color=Locality)) +
+ggplot(data=s, aes(x=PC1, y=PC2)) +
   geom_point() + 
   # use viridis colors
   scale_color_viridis_d() +
   # use manual colors (need to define a color vector before then)
-  #scale_color_manual(values = c("red", "blue", "green", "orange")) +
   xlab(paste("PC1 (", round(v[1], 2), " %)")) +
   ylab(paste("PC2 (", round(v[2], 2), " %)")) +
   theme_bw() +
@@ -54,19 +31,4 @@ ggplot(data=s, aes(x=PC1, y=PC2, color=Locality)) +
         panel.grid.minor = element_blank(),
         axis.line = element_line(colour= "black"),
         legend.justification = c(0, 1))
-
-ggplot(data=s, aes(x=PC1, y=PC2, color=Locality, shape=Haplotype)) +
-  geom_point() + 
-  # use viridis colors
-  #scale_color_viridis_d() +
-  # use manual colors (need to define a color vector before then)
-  scale_color_manual(values = c("red", "blue", "green", "orange", "black")) +
-  xlab(paste("PC1 (", round(v[1], 2), " %)")) +
-  ylab(paste("PC2 (", round(v[2], 2), " %)")) +
-  theme_bw() +
-  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.line = element_line(colour= "black"),
-        legend.justification = c(0, 1))
-#cerrar el pdf que ya se creo
 dev.off()
